a1632686c0e8bd7bd8140bb7d60d31723e348e67
Febrian Amir <febrianamir5@gmail.com>
2022-05-28 22:10:56 +0000

    change fatal error to info if couldn't loading .env file

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ var (
 func init() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Println("Couldn't loading .env file")
 	}
 
 	ServerAddress = os.Getenv("SERVER_ADDRESS")
